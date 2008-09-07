cc2050cd2d26e02accba098f1c47fe378ed6e020
renesd <renesd@0ac3ed72-cf95-4e29-85cc-1417b15d12ac>
2008-09-07 23:46:58 +0000

    last nights coding.  added a basic intro.  made a Game class for each thing. started on a class to see if you do the notes in time.

--- trunk/intro.py
+++ trunk/intro.py
@@ -1,13 +1,18 @@
 
-
+import pygame
 from pygame.locals import *
 
 import game
 Game = game.Game
 
 
+
 class Intro(Game):
-    
+    def load(self):
+        Game.load(self)
+	self.font_size = 40
+	self.font = pygame.font.Font(None, self.font_size)
+	self.font_color = (255,255,255,255)
 
 
     def handle_events(self, events):
@@ -16,14 +21,44 @@ class Intro(Game):
 	for e in events:
 	    if e.type == KEYDOWN:
 	        if e.key == K_ESCAPE:
-		    self.going = False
+		    self.stop()
         
     def update(self, elapsed_time):
         Game.update(self, elapsed_time)
-        
-        
+
+	if self.elapsed_time < 2.0:
+	    self.text_string = "Eye stabs.   Do you?"
+	elif self.elapsed_time >= 2.0 and self.elapsed_time < 4.0:
+	    self.text_string = "Yeah I do stabs."
+	  
+	elif self.elapsed_time >= 4.0 and self.elapsed_time < 8.0:
+	    self.text_string = "Cool.  I like you then."
+
+	elif self.elapsed_time >= 8.0 and self.elapsed_time < 800.0:
+	    self.stop()
+
+
+    def stop(self):
+        """
+	"""
+	print 'intro stopped'
+	self.going = False
+
     def draw(self, screen):
         rects = Game.draw(self, screen)
+	the_text = self.font.render(self.text_string, 1, self.font_color)
+
+	screen.fill((0,0,0,255))
+
+	x,y = screen.get_rect().center
+
+	x = x - (the_text.get_width() / 2)
+
+	r = screen.blit(the_text, (x,y))
+
+	# update the whole screen.
+	rects.extend(screen.get_rect())
+
 	return rects
 
 
