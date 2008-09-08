d2eb3f5c4d79b1a9793c31c708f5a777bf1be1a5
renesd <renesd@0ac3ed72-cf95-4e29-85cc-1417b15d12ac>
2008-09-08 07:39:08 +0000

    fun with lines

--- trunk/intro.py
+++ trunk/intro.py
@@ -10,56 +10,69 @@ Game = game.Game
 class Intro(Game):
     def load(self):
         Game.load(self)
-	self.font_size = 40
-	self.font = pygame.font.Font(None, self.font_size)
-	self.font_color = (255,255,255,255)
+        self.font_size = 40
+        self.font = pygame.font.Font(None, self.font_size)
+        self.font_color = (255,255,255,255)
 
+        self.draw_lines = 0
 
     def handle_events(self, events):
         Game.handle_events(self, events)
 
-	for e in events:
-	    if e.type == KEYDOWN:
-	        if e.key == K_ESCAPE:
-		    self.stop()
+        for e in events:
+            if e.type == KEYDOWN:
+                if e.key == K_ESCAPE:
+                    self.stop()
         
     def update(self, elapsed_time):
         Game.update(self, elapsed_time)
 
-	if self.elapsed_time < 2.0:
-	    self.text_string = "Eye stabs.   Do you?"
-	elif self.elapsed_time >= 2.0 and self.elapsed_time < 4.0:
-	    self.text_string = "Yeah I do stabs."
-	  
-	elif self.elapsed_time >= 4.0 and self.elapsed_time < 8.0:
-	    self.text_string = "Cool.  I like you then."
+        if self.elapsed_time < 2.0:
+            self.text_string = "Eye stabs.   Do you?"
+        elif self.elapsed_time >= 2.0 and self.elapsed_time < 4.0:
+            self.text_string = "Yeah I do stabs."
+            self.draw_lines = 1
+          
+        elif self.elapsed_time >= 4.0 and self.elapsed_time < 8.0:
+            self.text_string = "Cool.  I like you then."
 
-	elif self.elapsed_time >= 8.0 and self.elapsed_time < 800.0:
-	    self.stop()
+        elif self.elapsed_time >= 8.0 and self.elapsed_time < 20.0:
+            self.draw_lines = 1
+            pass
+        elif self.elapsed_time >= 20.0 and self.elapsed_time < 800.0:
+            self.stop()
+
 
 
     def stop(self):
         """
-	"""
-	print 'intro stopped'
-	self.going = False
+        """
+        print 'intro stopped'
+        self.going = False
 
     def draw(self, screen):
         rects = Game.draw(self, screen)
-	the_text = self.font.render(self.text_string, 1, self.font_color)
+        the_text = self.font.render(self.text_string, 1, self.font_color)
 
-	screen.fill((0,0,0,255))
+        screen.fill((0,0,0,255))
 
-	x,y = screen.get_rect().center
+        x,y = screen.get_rect().center
 
-	x = x - (the_text.get_width() / 2)
+        x = x - (the_text.get_width() / 2)
 
-	r = screen.blit(the_text, (x,y))
+        r = screen.blit(the_text, (x,y))
 
-	# update the whole screen.
-	rects.extend(screen.get_rect())
+        if self.draw_lines:
+            for mult in range(5,30):
+                y = int(self.elapsed_time * mult)
+                pygame.draw.line(screen, (255,255,255,255), (0,y), (screen.get_width()-1, y), 5)
 
-	return rects
+
+
+        # update the whole screen.
+        rects.extend(screen.get_rect())
+
+        return rects
 
 
 
