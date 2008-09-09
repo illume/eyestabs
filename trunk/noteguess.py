7a3bda270efff1ff03a5fedf6c1116a58b70e57e
renesd <renesd@0ac3ed72-cf95-4e29-85cc-1417b15d12ac>
2008-09-09 23:09:27 +0000

    can now show a song, and be able to tap out the notes.

--- trunk/noteguess.py
+++ trunk/noteguess.py
@@ -1,11 +1,16 @@
 from pygame.locals import *
 import pygame
+
+
 from game import Game
+import songs
 
 
 # Show notes on the screen, and people have to tap keyboard in time with notes.
 
 # notes_time = [["a", 0.0+s], ["c", 2.0+s], ["b", 4.0+s], ["c", 7.0+s], ]
+
+
 
 
 # how do we do sharps/flats on a computer keyboard?
@@ -30,10 +35,22 @@ class NoteGuess(Game):
 	
 
 
-    def draw(self, screen):
-        """
-	"""
+        self.font_size = 40
+        self.font = pygame.font.Font(None, self.font_size)
+        self.font_color = (255,255,255,255)
+        self.font_color = (0,0,0,255)
 
+
+        # the song currently being played.
+        self.current_song = songs.Song()
+
+        # a dummy song.
+
+        s = 2.0
+        self.current_song.notes_time = [["a", 0.0+s], ["c", 2.0+s], ["b", 4.0+s], ["c", 7.0+s], ]
+
+
+    def draw(self, screen):
 	"""
 		- from [[note,time], ...] 
 		- display notes across the screen.   
@@ -49,23 +66,34 @@ class NoteGuess(Game):
 	    print self.notes_time
 
 
-
-
 	screen.fill((255, 255, 255,255))
         
         
 	# draw the representation of time.  
 	#     A line from top to bottom of screen, that moves.
 
+        self.screen_width = screen.get_width()
+	time_line_x = self.get_position_for_time(self.elapsed_time)
 
-        time_line_x = self.elapsed_time * (screen.get_width() / self.total_time)
-	
 	pygame.draw.line(screen, 
 	                 (255,0,0,255), (time_line_x, 0), 
 	                 (time_line_x, screen.get_height()), 1)
 
 
-        #TODO: draw the position of notes time
+
+
+
+        # draw the position of played notes time
+        text_y = screen.get_rect().center[1]
+        self.draw_notes_time(self.notes_time, text_y, screen)
+
+
+
+        # draw the notes we need to play.
+        self.draw_notes_time(self.current_song.notes_time, text_y + 50, screen)
+
+
+
 
 
 
@@ -75,9 +103,43 @@ class NoteGuess(Game):
         # update the whole screen.
 	rects.append( screen.get_rect() )
 	return rects
+
         
+
+
+    def draw_notes_time(self, notes_time, text_y, screen):
+        rects = []
+
+        for n,t in notes_time:
+
+            # the left of the letter is where it is.  
+            #    Should it be the center of the letter?
+            text_x = self.get_position_for_time(t)
+
+            the_text = self.font.render(n, 1, self.font_color)
+
+            text_blit_r = screen.blit(the_text, (text_x,text_y))
+            rects.append(text_blit_r)
+
+            line_color = (0,0,255,255)
+            r = pygame.draw.line(screen, 
+                                 line_color, (text_x, text_blit_r.bottom), 
+                                 (text_x, text_blit_r.bottom - 5), 1)
+            rects.append(r)
+
+        return rects
+
+
+
+    def get_position_for_time(self, some_time):
+        """ given some time, return the position on the screen for
+              that time.
+        """
+        time_line_x = some_time * (self.screen_width / self.total_time)
+
+        return time_line_x
         
-        
+
     def handle_events(self, events):
         Game.handle_events(self, events)
 
@@ -109,6 +171,10 @@ class NoteGuess(Game):
 
 	# process any input notes 
 	self.update_notes(self.elapsed_time)
+
+
+
+
 
 
 
