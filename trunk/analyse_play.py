0a962839cfe9ba5e2b3940baffde9d7b7bf809e2
ndudfield <ndudfield@0ac3ed72-cf95-4e29-85cc-1417b15d12ac>
2008-09-10 05:36:39 +0000

    analyse_thread

--- trunk/analyse_play.py
+++ trunk/analyse_play.py
@@ -1,17 +1,13 @@
 ################################################################################
 # STD LIBS
 
-import threading
-import time
+import sys
 
 # 3RD PARTY LIBS
 
 import numpy
 import pyaudio
 import analyse
-
-import pygame
-import pygame.event as event
 
 # USER LIBS
 
@@ -33,7 +29,7 @@ MINIMUM_VOLUME     = -15
 # Typical very loud sounds are -1dB and typical silence is -36dB.
 
 # note must be X decibels louder than previous to count as new note
-ATTACK_DELTA       =  1.5
+ATTACK_THRESHOLD       =  2
 
 # X midi notes, semitones
 OCTAVE_CORRECTION  =  12
@@ -43,96 +39,55 @@ SAMPLE_SIZE        =  1024
 
 ################################################################################
 
-class PitchDectectThread(threading.Thread):
-    def __init__(self, *args, **kw):
-        threading.Thread.__init__(self)
-        self._args = args
-        self._kw = kw
-
-    def init_audio(self):
-        # XXX SHOULD THIS BE MOVED OUT? pyaud reference passed in at __init__?
-        pyaud = pyaudio.PyAudio()
-
-        self.stream = pyaud.open ( *self._args, **self._kw )
-
-    def post_note(self, note, t, d, v):
-        for _ in range(5):
-            try:
-                pygame.fastevent.post (
-                    event.Event (
-                        PITCH_DETECT, 
-                        note = note,
-                        time = t, 
-                        delta = d,
-                        volume = v,
-                    )
-                )
-                break
-            except Exception, e:
-                pass
-
-    def run(self):
-        self.init_audio()
-
-        last_note = last_vol = last_time = 0
-        
-        volumes = []
-        
-        while True:
-            # Read raw data
-            rawsamps = self.stream.read(SAMPLE_SIZE)
-            t = self.stream.get_time()
-
-            # Convert raw data to NumPy array
-            samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
-            
-            midi_note = analyse.musical_detect_pitch(samps, min_note=25.0)
-        
-            if midi_note:
-                midi_note += OCTAVE_CORRECTION
-            
-                latest_note = notes.midi_to_note(midi_note)        
-                latest_vol = analyse.loudness(samps)
-                
-                attacked = latest_vol - last_vol > ATTACK_DELTA
-
-                if latest_note != last_note or attacked:
-                    if latest_vol > MINIMUM_VOLUME:
-                        self.post_note(latest_note, t, t - last_time, latest_vol)
-                        
-                        
-                    last_note = latest_note
-                    last_vol = latest_vol
-                    last_time = t
-            
-            elif last_note:
-                last_note = None
-
-################################################################################
-
-def example():
-    pygame.init()
-    pygame.fastevent.init()
-
-    t = PitchDectectThread (
+def main():
+    pyaud = pyaudio.PyAudio()
+    
+    stream = pyaud.open (
         format = pyaudio.paInt16,
         channels = 2,
         rate = 44100,
         input_device_index = 1,
         input = True 
     )
-
-    t.setDaemon(1)
-    t.start()
-
+    
+    last_note = last_vol = last_time = 0
+    
+    volumes = []
+    
+    log = open('fuck.txt', 'a')
+        
     while True:
-        events = pygame.fastevent.get()
-        for e in events:
-            print e
+        # Read raw data
+        rawsamps = stream.read(SAMPLE_SIZE)
+        t = stream.get_time()
+    
+        # Convert raw data to NumPy array
+        samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
+    
+        midi_note = analyse.musical_detect_pitch(samps, min_note=28.0)
+    
+        if midi_note:
+            midi_note += OCTAVE_CORRECTION
+    
+            latest_note = notes.midi_to_note(midi_note)       
+            latest_vol = analyse.loudness(samps)
+    
+            attacked = latest_vol - last_vol > ATTACK_THRESHOLD
+    
+            if latest_note != last_note or attacked:
+                if latest_vol > MINIMUM_VOLUME:
+                    delta = t - last_time
 
-################################################################################
+                    print repr({'note':latest_note, 'time': t})
+                    sys.stdout.flush()
+
+                    last_time = t
+
+                last_note = latest_note
+                last_vol = latest_vol
+    
+        elif last_note:
+            last_note = None
 
 if __name__ == '__main__':
-    example()
-
-################################################################################
+    main()
--- trunk/noteguess.py
+++ trunk/noteguess.py
@@ -1,16 +1,12 @@
 from pygame.locals import *
 import pygame
 
-
 from game import Game
 import songs
-
 
 # Show notes on the screen, and people have to tap keyboard in time with notes.
 
 # notes_time = [["a", 0.0+s], ["c", 2.0+s], ["b", 4.0+s], ["c", 7.0+s], ]
-
-
 
 
 # how do we do sharps/flats on a computer keyboard?
@@ -25,14 +21,14 @@ class NoteGuess(Game):
         self.notes_to_press = []
         
         # [[note, elapsed_time], ...]
-	self.notes_time = []
+        self.notes_time = []
         
-	self.elapsed_time = 0.0
+        self.elapsed_time = 0.0
         
         
         # total time allowed in the song.
-	self.total_time = 30.
-	
+        self.total_time = 30.
+        
 
 
         self.font_size = 40
@@ -51,33 +47,33 @@ class NoteGuess(Game):
 
 
     def draw(self, screen):
-	"""
-		- from [[note,time], ...] 
-		- display notes across the screen.   
-		    - total time, eg 30.0 seconds - divided by screen width (640)
-			- draw a note at the pos, or an underline.
-
-		- display position of time.  As a line from top of screen to bottom.
         """
-	rects = []
+                - from [[note,time], ...] 
+                - display notes across the screen.   
+                    - total time, eg 30.0 seconds - divided by screen width (640)
+                        - draw a note at the pos, or an underline.
 
-	if self.notes_time:
-	    # the notes that people press.
-	    print self.notes_time
+                - display position of time.  As a line from top of screen to bottom.
+        """
+        rects = []
+
+        if self.notes_time:
+            # the notes that people press.
+            print self.notes_time
 
 
-	screen.fill((255, 255, 255,255))
+        screen.fill((255, 255, 255,255))
         
         
-	# draw the representation of time.  
-	#     A line from top to bottom of screen, that moves.
+        # draw the representation of time.  
+        #     A line from top to bottom of screen, that moves.
 
         self.screen_width = screen.get_width()
-	time_line_x = self.get_position_for_time(self.elapsed_time)
+        time_line_x = self.get_position_for_time(self.elapsed_time)
 
-	pygame.draw.line(screen, 
-	                 (255,0,0,255), (time_line_x, 0), 
-	                 (time_line_x, screen.get_height()), 1)
+        pygame.draw.line(screen, 
+                         (255,0,0,255), (time_line_x, 0), 
+                         (time_line_x, screen.get_height()), 1)
 
 
 
@@ -98,11 +94,11 @@ class NoteGuess(Game):
 
 
         # clear the notes that we've processed.
-	#self.notes_time = []
+        #self.notes_time = []
 
         # update the whole screen.
-	rects.append( screen.get_rect() )
-	return rects
+        rects.append( screen.get_rect() )
+        return rects
 
         
 
@@ -143,14 +139,14 @@ class NoteGuess(Game):
     def handle_events(self, events):
         Game.handle_events(self, events)
 
-	notes_pressed = []
+        notes_pressed = []
 
-	for e in events:
-	    if e.type == KEYDOWN:
-	        
-	        if e.unicode in NOTES: 
-		    print "HIIH!!"
-		    notes_pressed.append(e.unicode)
+        for e in events:
+            if e.type == KEYDOWN:
+                
+                if e.unicode in NOTES: 
+                    print "HIIH!!"
+                    notes_pressed.append(e.unicode)
 
         self.notes_last_pressed = notes_pressed
 
@@ -158,25 +154,16 @@ class NoteGuess(Game):
     def update_notes(self, elapsed_time):
         # translate the notes into [[note, elapsed_time], ]
         all_notes = []
-	for note in self.notes_last_pressed:
+        for note in self.notes_last_pressed:
             all_notes.append( [note, elapsed_time] )
 
-	self.notes_last_pressed = []
+        self.notes_last_pressed = []
 
-	self.notes_time.extend(all_notes)
+        self.notes_time.extend(all_notes)
 
 
     def update(self, elapsed_time):
         Game.update(self, elapsed_time)
 
-	# process any input notes 
-	self.update_notes(self.elapsed_time)
-
-
-
-
-
-
-
-
-
+        # process any input notes 
+        self.update_notes(self.elapsed_time)
