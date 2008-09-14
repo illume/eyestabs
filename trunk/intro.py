0524d25ebb381a61d8f7fd2924e2a1a777434fa5
renesd <renesd@0ac3ed72-cf95-4e29-85cc-1417b15d12ac>
2008-09-14 02:04:11 +0000

    fixed up second part of intro...

--- trunk/intro.py
+++ trunk/intro.py
@@ -16,6 +16,10 @@ class Intro(Game):
         self.font = pygame.font.Font(data.filepath("freesansbold.ttf"), self.font_size)
         self.font_color = (255,255,255,255)
 
+        self.font_size2 = 20
+        self.font2 = pygame.font.Font(data.filepath("freesansbold.ttf"), self.font_size2)
+        self.font_color2 = (255,255,255,255)
+
         # should we be drawing the line element of the intro?
         self.draw_lines = 0
 
@@ -39,7 +43,6 @@ class Intro(Game):
     def update(self, elapsed_time):
         Game.update(self, elapsed_time)
 
-        print 'updating...'
 
         if self.elapsed_time < 2.0:
             self.text_string = "Eye stabs.   Do you?"
@@ -54,17 +57,16 @@ class Intro(Game):
             pass
 
 
-        elif self.elapsed_time >= 24.0 and self.elapsed_time < 800.0:
+        elif self.elapsed_time >= 24.0 and self.elapsed_time < 50.0:
 
             self.second_text = 1
             self.first_text = 0
             self.draw_lines = 0
 
-            print 'doit...'
 
 
 
-        elif self.elapsed_time >= 900.0 and self.elapsed_time < 1000.0:
+        elif self.elapsed_time >= 50.0 and self.elapsed_time < 1000.0:
             #self.stop()
             data.where_to = "gig_select"
 
@@ -111,24 +113,14 @@ class Intro(Game):
 
         if self.second_text:
 
-            intro_text = """
-<SCARY DARK SILLY VOICE OVER VOICE>
-In the dirty underground music scene often referred to as 'eye stabs', there are lethal gigs in select nightclubs around the world.  Musicians play for eyes. A tune is played to the musician, and they must figure out the notes played.
+            intro_text = """<SCARY DARK SILLY VOICE OVER VOICE> In the dirty underground music scene often referred to as 'eye stabs', there are lethal gigs in select nightclubs around the world.  Musicians play for eyes. A tune is played to the musician, and they must figure out the notes played.        Fuck up the tune, and you are stabbed in the eye! Get the tune right, then fortune and respect are yours! Better than the riches and the adoration though -- rich club owners can give you spare eyes... and provide doctors that can give you back eyes you have had stabbed out.        So are you prepared to put your eyes on the line? Can you play well enough to avoid a stab in the eye? Do you trust the sleazy club owners doctors to fix your eye if it does get stabbed?        </SCARY DARK SILLY VOICE OVER VOICE>"""
 
-Fuck up the tune, and you are stabbed in the eye! Get the tune right, then fortune and respect are yours! Better than the riches and the adoration though -- rich club owners can give you spare eyes... and provide doctors that can give you back eyes you have had stabbed out.
 
-So are you prepared to put your eyes on the line? Can you play well enough to avoid a stab in the eye? Do you trust the sleazy club owners doctors to fix your eye if it does get stabbed? 
-
-</SCARY DARK SILLY VOICE OVER VOICE>
-            """
-
-            #print self.elapsed_time
-
-            text_lines = textwrap.wrapline(intro_text, self.font, 630)
-            print text_lines
+            text_lines = textwrap.wrapline(intro_text, self.font2, 630)
             new_lines = []
             for l in text_lines:
-                new_lines.extend( l.split("\n") )
+                pass
+                new_lines.extend( l.split("        ") )
 
             text_lines = new_lines
             
@@ -136,19 +128,20 @@ So are you prepared to put your eyes on the line? Can you play well enough to av
 
 
             for i, text_string in enumerate(text_lines):
-                the_text = self.font.render(text_string, 1, self.font_color)
+                the_text = self.font2.render(text_string, 1, self.font_color2)
 
                 # draw the text in the middle of the screen.
                 text_x = 5
                 start_time = 25.
-                time_speed = (self.elapsed_time - start_time) * 14
-                #print time_speed
-                text_y = ((the_text.get_height() * 1.2 *  i) - time_speed)
-                #print text_y
+                el = self.elapsed_time
+                if el > 40.:
+                    el = 40.
+
+                time_speed = (el - start_time) * 14
+                text_y = (screen.get_height()/2.) + ((the_text.get_height() * 1.2 *  i) - time_speed)
 
                 if text_y + the_text.get_height() < screen.get_height():
                     if text_y + the_text.get_height() > 0:
-
                         r = screen.blit(the_text, (text_x,text_y))
 
 
@@ -158,7 +151,7 @@ So are you prepared to put your eyes on the line? Can you play well enough to av
 
 
         # update the whole screen.
-        rects.extend(screen.get_rect())
+        rects.extend([screen.get_rect()])
 
         return rects
 
